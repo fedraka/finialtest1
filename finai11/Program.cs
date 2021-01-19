0530917454c10785c4499df2b66037ea71ea4095
FKarshenas <FKarshenas@RP-HARTS05>
2021-01-18 20:43:53 -0500

    first commit

--- finai11/Program.cs
+++ finai11/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
 
-namespace finai11
+
+
+public class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	public static void Main()
+	{
+		var person = new Person();
+		person.personName = "test";
+
+
+		var alarm = new AlarmClock();
+		alarm.AlarmClockEvent += person.HandleAlarm;
+		alarm.Alarm();
+	}
+}
+
+public class alarmClockEventArgs : EventArgs
+{
+	public DateTime datetime { set; get; }
+	public alarmClockEventArgs(DateTime datetime)
+	{
+		this.datetime = datetime;
+	}
+}
+
+public delegate void AlarmClockHandeler(object source, alarmClockEventArgs e);
+
+public class AlarmClock
+{
+	public event AlarmClockHandeler AlarmClockEvent;
+	public void Alarm()
+	{
+		AlarmClockHandeler alarm = AlarmClockEvent;
+		if (AlarmClockEvent != null)
+		{
+			alarm(this, new alarmClockEventArgs(DateTime.Now));
+		}
+	}
+}
+
+public class Person
+{
+	public string personName { get; set; }
+
+	public void HandleAlarm(object sender, alarmClockEventArgs e)
+	{
+		Console.WriteLine("Alarm Go off!!!!");
+	}
 }
